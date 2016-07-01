c5c1df41d5861f3232233ebc3f22e4682357ab90
Gary Murphy <compsciguy.it@gmail.com>
2016-07-01 16:26:25 +1000

    Recreated in VS2015

---
+++ CompleteBeginner/CompleteBeginner/CompleteBeginner.cpp
@@ -0,0 +1,41 @@
+// CompleteBeginner.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World! \n" << endl;
+
+	for (int i = 1; i <= 12; i++)
+	{
+		cout << i << " x 12 = " << i * 12 << "\n";
+	}
+	cout << endl;
+
+	int timesTable[10][10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			timesTable[i][j] = i * j;
+			cout << "timesTable[" << i << "] * [" << j << "] = " << timesTable[i][j] << endl;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << "timesTable[" << i << "][" << j << "] = " << timesTable[i][j] << endl;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
